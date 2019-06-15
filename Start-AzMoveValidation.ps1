6544ae64c03ee8817365e58159dfc1a3e750276e
Tobias Vilhelmsen <tobias.vilhelmsen@sentia.dk>
2019-06-15 21:23:29 +0200

    Adds help

--- Start-AzMoveValidation.ps1
+++ Start-AzMoveValidation.ps1
@@ -1,3 +1,14 @@
+<#
+.Synopsis
+   Start-AzMoveValidation.ps1 - Validates whether resources can be moved from one resource group to another resource group.
+.DESCRIPTION
+   This function checks whether the specified resources can be moved to the target. The resources to move must be in the same source resource group. The target resource group may be in a different subscription
+.EXAMPLE
+   Start-AzMoveValidation -TargetSubscriptionId 'SubscriptionId' -TargetResourceGrup 'Resource group name' -SourceSubscriptionId 'SubscriptionId' -SourceResourceGroup 'Resource group name' -SourceTenantId 'TenantId'
+.NOTES
+   Written by Tobias Vilhelmsen
+#>
+
 function Start-AzMoveValidation {
     
     param (
